a40f81f9c4a5e612ce2273a8f2de37121c2f7c7c
Mohith Manubarthi <mohithias24@gmail.com>
2025-04-29 10:36:07 +0530

    Update postfix.c

--- postfix.c
+++ postfix.c
@@ -1,54 +1,55 @@
 #include<stdio.h>
 #include<ctype.h>
+
 int stack[20];
 int top = -1;
-void push(int x)
-{
-stack[++top] = x;
-}
-int pop()
-{
-return stack[top--];
-}
-int main()
-{
-char exp[20];
-char *e;
-int n1,n2,n3,num;
-printf("Enter the expression :: ");
-scanf("%s",exp);
-e = exp;
-while(*e != '\0')
-{
-if(isdigit(*e))
-{
-num = *e - 48;
-push(num);
-}
-else
-{
-n1 = pop();
-n2 = pop();
-switch(*e)
-{
-case '+':
-n3 = n1 + n2;
-break;
-case '-':
-n3 = n2 - n1;
 
-break;
-case '*':
-n3 = n1 * n2;
-break;
-case '/':
-n3 = n2 / n1;
-break;
+void push(int x) {
+    stack[++top] = x;
 }
-push(n3);
+
+int pop() {
+    return stack[top--];
 }
-e++;
-}
-printf("\nThe result of expression %s = %d\n\n",exp,pop());
-return 0;
+
+int main() {
+    char exp[20];
+    char *e;
+    int n1, n2, n3, num;
+
+    printf("Enter the postfix expression: ");
+    scanf("%s", exp);
+
+    e = exp;
+    while(*e != '\0') {
+        if(isdigit(*e)) {
+            num = *e - '0'; // better than using 48
+            push(num);
+        } else {
+            n1 = pop();
+            n2 = pop();
+            switch(*e) {
+                case '+':
+                    n3 = n2 + n1;
+                    break;
+                case '-':
+                    n3 = n2 - n1;
+                    break;
+                case '*':
+                    n3 = n2 * n1;
+                    break;
+                case '/':
+                    n3 = n2 / n1;
+                    break;
+                default:
+                    printf("Invalid operator %c\n", *e);
+                    return 1; // error
+            }
+            push(n3);
+        }
+        e++;
+    }
+
+    printf("\nThe result of postfix expression %s = %d\n", exp, pop());
+    return 0;
 }
